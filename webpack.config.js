241cf770a069bc98043d6fefea9ba40782a2df87
vldchz <w.chizhenkov@yandex.ru>
2022-02-23 21:19:40 +0400

    add file-loader, edit webpack config for use fonts

--- webpack.config.js
+++ webpack.config.js
@@ -41,6 +41,18 @@ module.exports = {
                         loader: "pug-loader",
                     },
                 ],
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            name:'[name].[ext]',
+                            outputPath: 'fonts/'
+                        }
+                    }
+                ]
             }
         ]
     },
