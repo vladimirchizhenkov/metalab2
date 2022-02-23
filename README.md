2b7de22d62de00cb47a2e55d0c064b9d03c3c887
vldchz <w.chizhenkov@yandex.ru>
2022-02-23 19:48:22 +0400

    add clean dist after each iteration

---
+++ README.md
@@ -0,0 +1,5 @@
+### Node & npm
+Used node 12.22.5, npm 7.5.2
+
+### Ссылка на шрифты 
+Montserrat and Quicksand https://fonts.google.com/share?selection.family=Montserrat:wght@400;700%7CQuicksand:wght@400;700
--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,8 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, './dist'),
-        filename: '[name].bundle.js'
+        filename: '[name].bundle.js',
+        clean: true
     },
     mode: 'development',
     devServer: {
